d604a51ad8adccbef911171b829a6be7c01e8f32
Jacob Rush <psyjr14@nottingham.ac.uk>
2026-09-05 01:49:12 +0100

    feat: meals and images displayed; rate limiting for area dropdown

--- backend/main.py
+++ backend/main.py
@@ -33,16 +33,16 @@ async def list_areas():
 async def search_areas(query: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{MEALDB_BASE}/list.php", params=({"a": "list"}))
-    areas = response.json().get("meals", [])    #list of all areas
+        areas = response.json().get("meals", [])    #list of all areas
 
     matches = [area for area in areas
                if query.lower() in area["strArea"].lower()]
 
     return {"matches": matches}
 
-@app.get("areas/{area}/meals")
+@app.get("/areas/{area}/meals")
 async def get_meals(area: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{MEALDB_BASE}/filter.php", params=({"a": area}))
     meals = response.json().get("meals") or []  #[] if no meals
-    return {"meals: meals"}
+    return {"meals": meals}
--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 const areaInput = document.getElementById("area1");
 const dropdown = document.getElementById("dropdown-div");
+let debouncer;
 
 async function searchArea(query) {
   const response = await fetch(
@@ -32,17 +33,48 @@ function displayDropdown(results){
         item.addEventListener("click", () => {
             areaInput.value = result.strCountry;
             resultsDropdown.innerHTML = "";
+            getMealsByArea(result.strArea);
         });
         resultsDropdown.appendChild(item);
     });
 }
 
+async function getMealsByArea(area){
+    const response = await fetch(
+        `http://127.0.0.1:8000/areas/${encodeURIComponent(area)}/meals`
+    );
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch meals: ${response.status}`);
+    }
+
+    const result = await response.json();
+    displayMeals(result.meals);
+}
+
+function displayMeals(meals){
+    const mealsDiv = document.getElementById("meals");
+    mealsDiv.innerHTML = "";
+
+    meals.forEach((meal) => {
+        const item = document.createElement("div");
+        item.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" width="100">
+            <p>${meal.strMeal}</p>
+        `;
+        mealsDiv.appendChild(item);
+    });
+}
+
 areaInput.addEventListener('input', () =>{
+    clearTimeout(debouncer);
     const query = areaInput.value.toLowerCase();
 
      if (query.length === 0) {
         return;
      }
-     searchArea(query);
-})
+    debouncer = setTimeout(() => {
+        searchArea(query);
+     }, 500);   //500ms delay - rate limiting
+});
 
